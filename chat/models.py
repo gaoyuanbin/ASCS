4a55760ac5b6e9fd0dc0978b9129c7206138893b
gaoyuanbin <gaoyuanbin0305@gmail.com>
2025-02-24 20:21:41 +0800

    Version 1 basic server

---
+++ chat/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+
+
+class Post(models.Model):
+    message = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    emojis = None
+
+    def __str__(self):
+        return self.title
+
+
+from django.db import models
+
+# Create your models here.
